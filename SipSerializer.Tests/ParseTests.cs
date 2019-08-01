0c3ca91b1bdbd43ff141855babedadbb36bff273
Jacob True <jacfal.tech@protonmail.com>
2019-08-01 18:48:14 +0200

    Raw Sip message added, obsolete unit tests deleted

--- SipSerializer.Tests/ParseTests.cs
+++ SipSerializer.Tests/ParseTests.cs
@@ -9,22 +9,47 @@ namespace Javor.SipSerializer.Tests
 {
     public class ParseTests
     {
-        // [Theory]
-        // [MemberData(nameof(TestCases.TestData_SipRequests), MemberType = typeof(TestCases))]
-        // public void Deserialize_Sip_Request_Success(string sipRequest)
-        // {
-        //     // *** ARRANGE ***
-        //     var sw = new Stopwatch();
+        [Fact]
+        public void Read_Sip_Message_Success()
+        {
+            // *** ARRANGE ***
+            string sipTest = @"INVITE sip: 80362@algocloud.net SIP/ 2.0\r\n" 
+                            + "FROM: \"Test JPR\" < sip:+420225006110@algocloud.net >; tag = 80eea88d4a11e71dda58b0b03400\r\n"
+                            + "To: < sip:80362@algocloud.net >\r\n"
+                            + "CSeq: 1 INVITE\r\n"
+                            + "Call-ID: 9255ba2465765c49339cde4c46086852\r\n"
+                            + "Contact: \"Test JPR\" < sip:+420225006110@91.241.11.242:5060; transport = udp; gsid = 80eea88d - 4a11 - 4701 - 8bda - 58b0b0340000 >\r\n"
+                            + "Allow: INVITE, ACK, OPTIONS, BYE, CANCEL, SUBSCRIBE, NOTIFY, REFER, INFO, PRACK, PUBLISH, UPDATE\r\n"
+                            + "Supported: 100rel, histinfo, join, replaces, timer\r\n"
+                            + "Max-Forwards: 66\r\n"
+                            + "Via: SIP / 2.0 / UDP 91.241.11.242:5060; branch = z9hG4bK - s1632 - 001300246567 - 1--s1632 -\r\n"
+                            + "Accept-Language: en\r\n"
+                            + "Alert-Info: < cid:internal @algocloud.net>;avaya-cm-alert-type=internal\r\n"
+                            + "P-Asserted-Identity: \"Test JPR\" <sip:+420225006110@algocloud.net>\r\n"
+                            + "Session-Expires: 1800;refresher=uac\r\n"
+                            + "Min-SE: 1800\r\n"
+                            + "Remote-LegId: 462\r\n"
+                            + "Content-Type: application/sdp\r\n"
+                            + "User-to-User: 00FA080001008858BFD6BD;encoding=hex\r\n"
+                            + "P-AV-Message-Id: 1_1\r\n"
+                            + "Max-Breadth: 60\r\n"
+                            + "P-Charging-Vector: icid-value=\"AAS:50-8da8ee801e7114ab058da0c34b0\"\r\n"
+                            + "Content-Length: 258\r\n";
 
-        //     // *** ACT ***
-        //     sw.Start();
-        //     SipRequestMessage result = SipRequestMessage.CreateSipRequest(sipRequest);
-        //     sw.Stop();
+            RawSipMessage roSipMessage = new RawSipMessage(sipTest);
 
-        //     var test = sw.ElapsedMilliseconds;
+            // *** ACT ***
+            string resultFrom = roSipMessage.GetHeaderValue("FROM");
+            string resultVia = roSipMessage.GetHeaderValue("Via");
+            string resultChargingVector = roSipMessage.GetHeaderValue("P-Charging-Vector");
+            string resultContentLength = roSipMessage.GetHeaderValue("Content-Length");
 
-        //     // *** ASSERT ***
-        // }
+            // *** ACT ***
+            Assert.Equal(" \"Test JPR\" < sip:+420225006110@algocloud.net >; tag = 80eea88d4a11e71dda58b0b03400", resultFrom);
+            Assert.Equal(" SIP / 2.0 / UDP 91.241.11.242:5060; branch = z9hG4bK - s1632 - 001300246567 - 1--s1632 -", resultVia);
+            Assert.Equal(" icid-value=\"AAS:50-8da8ee801e7114ab058da0c34b0\"", resultChargingVector);
+            Assert.Equal(" 258", resultContentLength);
+        }
 
         [Theory]
         [MemberData(nameof(TestCases.TestData_SipResponses), MemberType = typeof(TestCases))]
@@ -37,7 +62,7 @@ namespace Javor.SipSerializer.Tests
 
             // *** ACT ***
             sw.Start();
-            SipReponseMessage result = SipReponseMessage.CreateSipResponse(sipResponse);
+            SipResponse result = SipResponse.CreateSipResponse(sipResponse);
             sw.Stop();
 
             var test = sw.ElapsedMilliseconds;
@@ -48,7 +73,7 @@ namespace Javor.SipSerializer.Tests
         {
             // *** ARRANGE ***
             StatusLine sl = new StatusLine(StatusCode.Ok);
-            SipReponseMessage sr = new SipReponseMessage(sl);
+            SipResponse sr = new SipResponse(sl);
             sr.AddHeader(@"From: ""100, 100"" <sip:+420225006100@algocloud.net>;tag=808849de3db6e61a53657e99dc300");
             sr.AddHeader("Call-ID: 2bbb9f20629f720f74adca5da4ad4052");
             sr.AddHeader("CSeq: 1 INVITE");
@@ -58,23 +83,6 @@ namespace Javor.SipSerializer.Tests
 
             // *** ACT ***
             string result = sr.ToString();
-        }
-
-        [Theory]
-        [InlineData("INVITE sip:80362@algocloud.net SIP/2.0")]
-        public void RequestLine_Deserialization_Serialization_Success(string requestLineString)
-        {
-            RequestLine deserialization = new RequestLine(requestLineString);
-            string serialization = deserialization.ToString();
-
-            Assert.Equal(requestLineString, serialization);
-        }
-
-        [Theory]
-        [InlineData("SIP/2.0 200 OK")]
-        public void Parse_RequestLine_Failed_Invalid_Input(string badRequestLineString)
-        {
-            Assert. Throws<SipParsingException>(() => new RequestLine(badRequestLineString));
         }
     }
 }
---
+++ SipSerializer/RawSipMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Javor.SipSerializer
+{
+    public class RawSipMessage
+    {
+        public string SipMessage { get; private set; }
+
+        public RawSipMessage(string sipMessage)
+        {
+            if (string.IsNullOrEmpty(sipMessage)) throw new ArgumentNullException("Cannt be null or empty");
+
+            SipMessage = sipMessage;
+        }
+
+        public string GetHeaderValue(string headerName)
+        {
+            return FindHeaderValue(SipMessage.AsSpan(), headerName).ToString();
+        }
+
+        private ReadOnlySpan<char> FindHeaderValue(ReadOnlySpan<char> sipMessage, string headerName)
+        {
+            int headerIndex = sipMessage.IndexOf(headerName.AsSpan());
+
+            if (headerIndex < 0)
+                return null;
+
+            int delimiterPosition = -1;
+            bool delimiterFound = false;
+            int endOfTheLinePosition = -1;
+            for (int i = headerIndex; i < sipMessage.Length; i++)
+            {
+                // find delimiter
+                if (!delimiterFound && sipMessage[i] == ':')
+                {
+                    delimiterPosition = i;
+                    delimiterFound = true;
+                }
+
+                // find end of the line
+                if (sipMessage[i] == '\r' && sipMessage[i + 1] == '\n')
+                {
+                    endOfTheLinePosition = i - 1;
+                    break;
+                }
+            }
+
+            return sipMessage.Slice(delimiterPosition + 1, endOfTheLinePosition - delimiterPosition);
+        }
+    }
+}
